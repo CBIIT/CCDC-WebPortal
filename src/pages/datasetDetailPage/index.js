3e0a135ae5480a9956c2b885530a3630b0b92982
Sana <ssharifi000@gmail.com>
2021-10-05 11:40:05 -0400

    Add breadcrumbs to dataset detail page

--- src/pages/datasetDetailPage/index.js
+++ src/pages/datasetDetailPage/index.js
@@ -1,9 +1,33 @@
 import React from "react";
+// import { Link } from "react-router-dom";
+import './datasetDetailPage.css';
 
-const DatasetDetailPage = () => (
-    <div>
-        <h2>Dataset Detail Page Content</h2>
-    </div>
-);
+const DatasetDetailPage = () => {
+    return (
+      <>
+        <div className="datasetBreadcrumbContainer">
+          <nav separator="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item"><a href="/">Home</a></li>
+              <li className="breadcrumb-item"><a href="/search">Search Catalog</a></li>
+              <li className="breadcrumb-item active" aria-current="page">Dataset: Dataset Name</li>
+            </ol>
+          </nav>
+        </div>
+        <div className="aboutDatasetContainer">
+          <div className="coreDataContainer">
+            <div className="coreDataLabel">Core Data</div>
+          </div>
+          <div className="additionalDataContainer">
+            <div className="additionalDataLabel">Additional Data</div>
+          </div>
+        </div>
+        <div>
+          <h2>Dataset Detail Page Content</h2>
+        </div>
 
-export default DatasetDetailPage;
+      </>
+    );
+  };
+
+  export default DatasetDetailPage;
