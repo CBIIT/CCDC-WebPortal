a3bfb5166d6be668f4c29bbf0f07e9a16f4ca982
Wei <ywwei85@gmail.com>
2021-10-08 10:41:55 -0400

    Correct api url if using relative path

--- src/api/participatingResourcesApi.js
+++ src/api/participatingResourcesApi.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from "./apiUtils";
 
-const baseUrl = `${process.env.REACT_APP_API_URL}/service/dataresources`;
+const baseUrl = `${process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : ''}/service/dataresources`;
 
 export function getLandingParticipatingResources() {
   return fetch(`${baseUrl}/landing`)
--- src/api/searchApi.js
+++ src/api/searchApi.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from "./apiUtils";
 
-const baseUrl = `${process.env.REACT_APP_API_URL}/service/datasets`;
+const baseUrl = `${process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : ''}/service/datasets`;
 
 export function searchCatalog(body) {
   /*
