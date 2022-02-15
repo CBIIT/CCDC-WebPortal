ef9f765b720ab5c74431f8f921003aa560b4bd4b
Wei <ywwei85@gmail.com>
2022-02-15 12:47:49 -0500

    Clear up display issue on Mac

--- src/pages/participatingResourcesPage/Filters/Filters.js
+++ src/pages/participatingResourcesPage/Filters/Filters.js
@@ -13,7 +13,6 @@ const FilterLabel = styled.div`
   padding: 10px 10px 5px 10px;
   color: #245284;
   font-weight: 700;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 1.4rem;
 
   span {
