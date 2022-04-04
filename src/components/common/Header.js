b6ca97d7f70da17f9daa57aa74636e7370305472
Wei <ywwei85@gmail.com>
2022-04-04 08:55:58 -0400

    Implement PODCAT-407 feature: mobile responsive home page_part 1

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useLocation, Link, useNavigate} from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
 import logo from '../../assets/img/CCDC_Logo.svg';
 
@@ -8,99 +7,26 @@ const LogoBanner = styled.div`
 `;
 
 const LogoContainer = styled.div`
-  width: 1250px;
   margin: 0 auto;
-`;
-
-const Logo = styled.img`
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-position: left;
   height: 100px;
-`;
 
-const SearchArea = styled.div`
-  float: right;
-  display: flex;
-  border: 2px solid #11B886;
-  border-radius: 18px;
-  padding: 5px 10px 5px 15px;
-  color: #11B886;
-  font-size: 1rem;
-  font-weight: 400;
-  margin-top: 30px;
-  margin-right: 20px;
-  line-height: 26.41px;
-`;
-
-const SearchIcon = styled.div`
-  color: #00996b;
-  font-size: 1.1rem;
-  font-weight: 600;
-  cursor: pointer;
-
-  svg:hover {
-    color: #11B886;
-  }
-`;
-
-const SearchInput = styled.input`
-  border: none;
-  // color: #11B886;
-  color: #00996b;
-  font-size: 1rem;
-  font-weight: 600;
-  width: 250px;
-  margin-left: 5px;
-
-  ::placeholder {
-    // color: #11B886;
-    color: #00996b;
-    font-size: 1rem;
-    font-weight: 600;
+  @media (min-width: 1200px) {
+    width: 1200px;
   }
 
-  :focus {
-    outline: none;
+  @media (max-width: 473px) {
+    background-position: center;
   }
 `;
 
 const Header = () => {
-  const path = useLocation().pathname;
-  const navigate = useNavigate();
-  const [localText, setLocalText] = useState("");
-
-  const handleTextInputChange = (event) => {
-    const text = event.target.value;
-    setLocalText(text);
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      navigate(`/sitesearch?keyword=${localText.trim()}`);
-      setLocalText("");
-    }
-  };
-
-  const handleSearch = () => {
-    navigate(`/sitesearch?keyword=${localText.trim()}`);
-    setLocalText("");
-  };
-
   return (
     <>
     <LogoBanner role="banner">
-      <LogoContainer>
-        <Link to="/"><Logo src={logo} alt="logo" /></Link>
-        {
-          path !== "/sitesearch"
-          && (
-          <SearchArea>
-            <SearchInput type="text" value={localText} placeholder="Documentation Search" onChange={handleTextInputChange} onKeyPress={handleKeyPress} />
-            <SearchIcon onClick={handleSearch}>
-              <i className="fas fa-search" />
-            </SearchIcon>
-          </SearchArea>
-          )
-        }
-      </LogoContainer>
+      <LogoContainer />
     </LogoBanner>
     </>
   );
