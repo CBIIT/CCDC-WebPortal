fa78da7f197a2ffe61dec844b50fa8166c1230d2
David-YuWei <ywwei85@gmail.com>
2022-06-08 20:59:06 -0400

    Implement PODCAT-426 feature: Adjust participating resource card display machanism on the home page

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import DataResourceIcons from '../DataResourceIcons';
 import './Carousel.css';
 import arrowRightGold from '../../assets/img/arrow_right_gold.svg';
-import arrowRightGray from '../../assets/img/arrow_right_gray.svg';
+
+let cardIdx = 0;
+let timer = null;
 
 const useShowCount = () => {
   const [showCount, setShowCount] = useState(3);
@@ -33,29 +35,52 @@ const Carousel = ({
     const [touchPosition, setTouchPosition] = useState(null);
     const [transform, setTransform] = useState({ transform: "translateX(0%)" });
 
+    const startTimer = () => {
+        timer = setInterval(() => {
+            const ts = `translateX(-${(cardIdx + 1) * (100 / showCount)}%)`;
+            cardIdx += 1;
+            setCurrentIndex(cardIdx);
+            setTransform({transform: ts, transition: ".5s ease-out"});
+        }, 3000);
+    };
+
+    const resetTimer = () => {
+        clearInterval(timer);
+        startTimer();
+    };
+
+    const nextSlide = () => {
+        resetTimer();
+        const ts = `translateX(-${(cardIdx + 1) * (100 / showCount)}%)`;
+        cardIdx += 1;
+        setCurrentIndex(cardIdx);
+        setTransform({transform: ts, transition: "0.5s ease-out"});
+    };
+
+    const prevSlide = () => {
+        resetTimer();
+        const ts = `translateX(-${(cardIdx - 1) * (100 / showCount)}%)`;
+        cardIdx -= 1;
+        setCurrentIndex(cardIdx);
+        setTransform({transform: ts, transition: "0.5s ease-out"});
+    };
+
     useEffect(() => {
-        if (participatingResources.length === 0) {
+        const len = participatingResources.length;
+        if (len === 0) {
             onLoadLandingParticipatingResources().catch(error => {
               throw new Error(`Loading landing page participating resources failed ${error}`);
             });
         }
-    }, []);
-
-    const next = () => {
-        if (currentIndex < (participatingResources.length - showCount)) {
-            setCurrentIndex(prevState => prevState + 1);
-            const ts = `translateX(-${(currentIndex + 1) * (100 / showCount)}%)`;
-            setTransform({transform: ts});
+        if (participatingResources.length !== 0) {
+            cardIdx = Math.floor(Math.random() * len) + showCount;
+            const initialTs = `translateX(-${cardIdx * (100 / showCount)}%)`;
+            setCurrentIndex(cardIdx);
+            setTransform({transform: initialTs, transition: "none"});
+            startTimer();
         }
-    };
-
-    const prev = () => {
-        if (currentIndex > 0) {
-            setCurrentIndex(prevState => prevState - 1);
-            const ts = `translateX(-${(currentIndex - 1) * (100 / showCount)}%)`;
-            setTransform({transform: ts});
-        }
-    };
+        return () => clearInterval(timer);
+    }, [participatingResources]);
 
     const handleTouchStart = (e) => {
         const touchDown = e.touches[0].clientX;
@@ -73,14 +98,30 @@ const Carousel = ({
         const diff = touchDown - currentTouch;
 
         if (diff > 5) {
-            next();
+            nextSlide();
         }
 
         if (diff < -5) {
-            prev();
+            prevSlide();
         }
 
         setTouchPosition(null);
+    };
+
+    const handleTransitionEnd = () => {
+        if (cardIdx === 0) {
+            cardIdx = 30;
+            const initialTs = `translateX(-${cardIdx * (100 / showCount)}%)`;
+            setCurrentIndex(cardIdx);
+            setTransform({transform: initialTs, transition: "none"});
+        }
+
+        if (cardIdx === (30 + showCount)) {
+            cardIdx = showCount;
+            const initialTs = `translateX(-${cardIdx * (100 / showCount)}%)`;
+            setCurrentIndex(cardIdx);
+            setTransform({transform: initialTs, transition: "none"});
+        }
     };
 
     return participatingResources.length === 0
@@ -93,31 +134,56 @@ const Carousel = ({
         <div className="carousel-container">
             <div className="carousel-wrapper">
                 <div className="carousel-action-wrapper">
-                {
-                    currentIndex > 0
-                     ? (
-                        <button type="button" onClick={prev} className="left-arrow">
-                            <img src={arrowRightGold} alt="arrow-right" />
-                        </button>
-                    )
-                    : (
-                        <button type="button" className="left-arrow" disabled="disabled">
-                            <img src={arrowRightGray} alt="arrow-right" />
-                        </button>
-                    )
-                }
+                    <button type="button" onClick={prevSlide} className="left-arrow">
+                        <img src={arrowRightGold} alt="arrow-right" />
+                    </button>
                 </div>
                 <div className="carousel-content-wrapper" onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
-                    <div className={`carousel-content show-${showCount}`} style={transform}>
+                    <div className={`carousel-content show-${showCount}`} onTransitionEnd={handleTransitionEnd} style={transform}>
+                    {participatingResources.slice(participatingResources.length - showCount).map((pr, idx) => {
+                            const key = `carousel_${idx}_last_clone`;
+                            let withLine = true;
+                            if (showCount === 1) {
+                                withLine = false;
+                            } else if (showCount === 2) {
+                                withLine = idx === currentIndex;
+                            } else {
+                                withLine = idx === currentIndex || (idx - 1) === currentIndex;
+                            }
+                            return (
+                                <div key={key} className={withLine ? "carousel-card-with-line" : "carousel-card"} title={key}>
+                                    <div className="cardTitle">
+                                        <div className="cardInfo">
+                                            <div className="cardLabel">
+                                                <span style={{ color: 'gray' }}> Participating Resources </span>
+                                            </div>
+                                            <div className="cardHeader">
+                                              <a className="cardHeader" href={`/resource/${pr.data_resource_id}`}><h4>{pr.resource_name.length > 55 ? `${pr.resource_name.substring(0, 55)} ...` : pr.resource_name}</h4></a>
+                                            </div>
+                                        </div>
+                                        <div className="cardIcon">
+                                          <DataResourceIcons participatingResource={pr.data_resource_id} type="white" />
+                                        </div>
+                                    </div>
+                                    <div className="cardContent">
+                                        <div>
+                                            {pr.description.length > 80 ? `${pr.description.substring(0, 80)}...` : pr.description}
+                                            {/* <span style={{ color: '#FFBF17' }}> READ MORE &#62; </span> */}
+                                            <span><a href={`/resource/${pr.data_resource_id}`} style={{ color: '#FFBF17', textDecoration: 'none' }}> READ MORE &#62; </a></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
                         {participatingResources.map((pr, idx) => {
                             const key = `carousel_${idx}`;
                             let withLine = true;
                             if (showCount === 1) {
-                              withLine = false;
+                                withLine = false;
                             } else if (showCount === 2) {
-                              withLine = idx === currentIndex;
+                                withLine = (idx + showCount) === currentIndex;
                             } else {
-                              withLine = idx === currentIndex || (idx - 1) === currentIndex;
+                                withLine = (idx + showCount) === currentIndex || (idx + showCount - 1) === currentIndex;
                             }
                             return (
                                 <div key={key} className={withLine ? "carousel-card-with-line" : "carousel-card"}>
@@ -144,22 +210,47 @@ const Carousel = ({
                                 </div>
                             );
                         })}
+                        {participatingResources.slice(0, showCount).map((pr, idx) => {
+                            const key = `carousel_${idx}_first_clone`;
+                            let withLine = true;
+                            if (showCount === 1) {
+                                withLine = false;
+                            } else if (showCount === 2) {
+                                withLine = (idx + 30 + showCount) === currentIndex;
+                            } else {
+                                withLine = (idx + 30 + showCount) === currentIndex || (idx + 30 + showCount - 1) === currentIndex;
+                            }
+                            return (
+                                <div key={key} className={withLine ? "carousel-card-with-line" : "carousel-card"} title={key}>
+                                    <div className="cardTitle">
+                                        <div className="cardInfo">
+                                            <div className="cardLabel">
+                                                <span style={{ color: 'gray' }}> Participating Resources </span>
+                                            </div>
+                                            <div className="cardHeader">
+                                              <a className="cardHeader" href={`/resource/${pr.data_resource_id}`}><h4>{pr.resource_name.length > 55 ? `${pr.resource_name.substring(0, 55)} ...` : pr.resource_name}</h4></a>
+                                            </div>
+                                        </div>
+                                        <div className="cardIcon">
+                                          <DataResourceIcons participatingResource={pr.data_resource_id} type="white" />
+                                        </div>
+                                    </div>
+                                    <div className="cardContent">
+                                        <div>
+                                            {pr.description.length > 80 ? `${pr.description.substring(0, 80)}...` : pr.description}
+                                            {/* <span style={{ color: '#FFBF17' }}> READ MORE &#62; </span> */}
+                                            <span><a href={`/resource/${pr.data_resource_id}`} style={{ color: '#FFBF17', textDecoration: 'none' }}> READ MORE &#62; </a></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
                 <div className="carousel-action-wrapper">
-                {
-                    currentIndex < (participatingResources.length - showCount)
-                     ? (
-                        <button type="button" onClick={next} className="right-arrow">
-                            <img src={arrowRightGold} alt="arrow-right" />
-                        </button>
-                     )
-                     : (
-                        <button type="button" className="right-arrow" disabled="disabled">
-                            <img src={arrowRightGray} alt="arrow-right" />
-                        </button>
-                     )
-                }
+                    <button type="button" onClick={nextSlide} className="right-arrow">
+                        <img src={arrowRightGold} alt="arrow-right" />
+                    </button>
                 </div>
             </div>
         </div>
