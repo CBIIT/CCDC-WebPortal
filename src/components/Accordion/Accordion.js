a5ff350e32121ba534a91a0fc34bc606df917b4d
David-YuWei <ywwei85@gmail.com>
2022-12-01 20:28:01 -0500

    Update Podcat-744 feature: fix collapse and expand UI issue (#207)
    * Update PODCAT-744 feature: fix participating resource detail page collapse issue
    
    * Update PODCAT-744 feature: fix participating resource page filter accordion issue

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Collapse from 'react-bootstrap/Collapse';
 import AccordionItem from './AccordionItem';
 import './Accordion.css';
 
@@ -9,6 +10,13 @@ const Accordion = ({
   selection,
 }) => {
   const displayCount = false;
+  const [open, setOpen] = useState(fields.map(() => true));
+
+  const handleClick = (idx) => {
+    const newOpen = Object.assign([], open);
+    newOpen[idx] = !newOpen[idx];
+    setOpen(newOpen);
+  };
 
   return (
     <>
@@ -16,18 +24,15 @@ const Accordion = ({
         {fields.map((field, idx) => {
           const str = field.toLowerCase().replaceAll(' ', '_');
           const key = `filters_${idx}`;
-          const stayOpenID = `so-${str}-${idx}`;
-          const collapseOneID = `co-${str}-${idx}`;
-          const defaultCollapsed = "show";
           // const firstAccordionClass = isResource ? "accordion-item-first" : "accordion-item";
           return (
             <div key={key} className="accordion-item">
-              <h2 className="accordion-header" id={stayOpenID}>
-                <button className={`accordion-button accordion-button-ccdc ${defaultCollapsed ? "" : "collapsed"}`} type="button" data-bs-toggle="collapse" data-bs-target={`#${collapseOneID}`} aria-expanded={defaultCollapsed ? "true" : "false"} aria-controls={collapseOneID}>
+              <h2 className="accordion-header">
+                <button type="button" onClick={() => handleClick(idx)} className={`accordion-button accordion-button-ccdc ${open[idx] ? "" : "collapsed"}`}>
                   {field}
                 </button>
               </h2>
-              <div id={collapseOneID} className={`accordion-collapse collapse ${defaultCollapsed ? "show" : ""}`} aria-labelledby={stayOpenID}>
+              <Collapse in={open[idx]}>
                 <div className="accordion-body">
                   {
                     filters[idx] && filters[idx].map((item, pos) => {
@@ -39,7 +44,7 @@ const Accordion = ({
                     })
                   }
                 </div>
-              </div>
+              </Collapse>
             </div>
           );
         })}
