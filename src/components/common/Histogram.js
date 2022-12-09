6b4ae4dd93a21216623e56d092e543b00f3b5866
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2022-12-09 13:16:34 -0500

    PODCAT-857: Dataset Detail page: Case age Diagnosis chart
    1. Used the following labels: "0 to 4," "5 to 9,"... "<40" (so ommitting the word "years" and 'Pediatric and Young Adult').
    2. Added a label to the x axis that reads "Age (years)" y axis "Cases"
    3. Changed the tooltip from "value" to "Cases"

--- src/components/common/Histogram.js
+++ src/components/common/Histogram.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import {
-  ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar
+  ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar, Label
 } from 'recharts';
 
 const Histogram = ({
     data
 }) => {
+    const [newData, setNewData] = useState([]);
+    useEffect(() => {
+      const newArr = [];
+      for (let i = 0; i < data.length; i += 1) {
+        const newItem = {};
+        if (data[i].name.includes("<40")) {
+          newItem.name = "<40";
+        } else if (data[i].name.toUpperCase().includes("GREATER")) {
+          newItem.name = ">39";
+        } else {
+          newItem.name = data[i].name.replace("years", "").trim();
+        }
+        newItem.Cases = data[i].value;
+        newArr.push(newItem);
+      }
+      setNewData(newArr);
+    }, [data]);
     return (
       <ResponsiveContainer height={300} width="100%">
-        <BarChart width={350} height={250} maxBarSize={80} data={data}>
+        <BarChart
+          width={350}
+          height={290}
+          maxBarSize={80}
+          data={newData}
+          margin={{left: 10, bottom: 20}}
+        >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis dataKey="name" tick={{fontSize: 13}}>
+              <Label value="Age (Years)" fill="#666" offset={-15} position="insideBottom" />
+            </XAxis>
+            <YAxis tick={{fontSize: 13}}>
+              <Label value="Cases" fill="#666" offset={-3} angle={-90} position="insideLeft" />
+            </YAxis>
             <Tooltip />
-            <Legend />
-            <Bar dataKey="value" fill="#00a272" />
+            <Bar dataKey="Cases" fill="#00a272" />
         </BarChart>
       </ResponsiveContainer>
     );
