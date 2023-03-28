b3a25d1fd38f2bc97c282dd6b23e2d880eef6381
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-03-28 14:31:45 -0400

    Added comma
    Added comma

--- src/pages/participatingResourcesPage/SearchResult/SearchResult.js
+++ src/pages/participatingResourcesPage/SearchResult/SearchResult.js
@@ -221,14 +221,20 @@ const SearchResult = ({
             <ResultInfo>No Results</ResultInfo>
           ) : resultList.map((rst, idx) => {
             const key = `sr_${idx}`;
-            const pocArray = rst.poc.split(" ; ");
-            const pocEmailArray = rst.poc_email.split(" ; ");
             const linkto = `/resource/${rst.data_resource_id}`;
             const linktoDatasetSummaries = `/resource/${rst.data_resource_id}#dataset_summaries`;
             const tooltip = tooltips[rst.resource_type.split(' ').join('')];
             let fullTitle = "".concat(rst.resource_name, ' (', rst.data_resource_id, ')');
             if (fullTitle.length > 77) {
               fullTitle = fullTitle.substring(0, 77).concat('...');
+            }
+            let pocArray = [];
+            let pocEmailArray = [];
+            if (rst.poc) {
+              pocArray = rst.poc.split(" ; ");
+            }
+            if (rst.poc_email) {
+              pocEmailArray = rst.poc_email.split(" ; ");
             }
             return (
               <ResourceCard key={key}>
@@ -264,7 +270,10 @@ const SearchResult = ({
                                   const pockey = `poc_${pocidx}`;
                                   const mailto = `mailto:${pocEmailArray[pocidx]}`;
                                   return (
-                                    <a key={pockey} href={mailto}>{pocitem}</a>
+                                    <>
+                                      <a key={pockey} href={mailto}>{pocitem}</a>
+                                      {pocidx !== pocArray.length - 1 && <span style={{color: '#00a272'}}>,</span>}
+                                    </>
                                   );
                                 })
                               }
