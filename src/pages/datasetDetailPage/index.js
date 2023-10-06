4efa5370a977bb1f73f6957ed9eb3b695a2cad08
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-10-06 16:33:47 -0400

    add glossaryTerms in dataset detail page

--- src/pages/datasetDetailPage/index.js
+++ src/pages/datasetDetailPage/index.js
@@ -3,14 +3,17 @@ import DatasetDetail from './DatasetDetail';
 import {
   loadDatasetDetail
 } from '../../redux/actions/searchActions';
+import { loadGlossaryTerms } from '../../redux/actions/applicationActions';
 
 const ReduxSearchResult = (() => {
   const mapStateToProps = (state) => ({
     details: state.datasets.details,
+    glossaryTerms: state.application.glossaryTerms,
   });
 
   const mapDispatchToProps = {
     onPageLoadDatasetDetail: loadDatasetDetail,
+    onLoadGlossaryTerms: loadGlossaryTerms,
   };
 
   return connect(mapStateToProps, mapDispatchToProps)(DatasetDetail);
