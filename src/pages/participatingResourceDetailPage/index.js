904b962855f9e689a102fbaa88eee98da72cd270
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-10-06 17:05:39 -0400

    add glossaryTerms in participant resource detail page

--- src/pages/participatingResourceDetailPage/index.js
+++ src/pages/participatingResourceDetailPage/index.js
@@ -4,16 +4,19 @@ import {
   loadDataresourceDetail,
   loadDataresourceDetailDatasets,
 } from '../../redux/actions/participatingResourcesActions';
+import { loadGlossaryTerms } from '../../redux/actions/applicationActions';
 
 const ReduxSearchResult = (() => {
   const mapStateToProps = (state) => ({
     detail: state.participatingResources.detail,
     datasets: state.participatingResources.datasets,
+    glossaryTerms: state.application.glossaryTerms,
   });
 
   const mapDispatchToProps = {
     onPageLoadDataresourceDetail: loadDataresourceDetail,
     onPageLoadDataresourceDetailDatasets: loadDataresourceDetailDatasets,
+    onLoadGlossaryTerms: loadGlossaryTerms,
   };
 
   return connect(mapStateToProps, mapDispatchToProps)(ParticipatingResourceDetail);
