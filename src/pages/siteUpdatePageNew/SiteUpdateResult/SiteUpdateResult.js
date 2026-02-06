6d2818107af368f8591b45b16523afdef155b241
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2026-02-06 15:04:37 -0500

    PODCAT-1686: Change link label for pdf download and open consolidated pdf in browser pdf viewer

--- src/pages/siteUpdatePageNew/SiteUpdateResult/SiteUpdateResult.js
+++ src/pages/siteUpdatePageNew/SiteUpdateResult/SiteUpdateResult.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Collapse from 'react-bootstrap/Collapse';
 import ReactHtmlParser from "html-react-parser";
-import html2pdf from "html2pdf.js";
+// import html2pdf from "html2pdf.js";
 import { OverlayTrigger, Popover, Spinner } from 'react-bootstrap';
-import NCILogoExport from "../../../assets/img/NCI_Logo.png";
+// import NCILogoExport from "../../../assets/img/NCI_Logo.png";
 import externalIcon from "../../../assets/img/resource-00a272.svg";
 import ClinicalTrialsIcon from '../../../assets/img/ClinicalTrials.icon.svg';
 import GenomicsIcon from '../../../assets/img/Genomics.icon.svg';
@@ -214,8 +214,10 @@ const SiteUpdateExport = styled.div`
     z-index: 9;
 
     .spanText {
-      padding-right: 15px;
+      padding-right: 8px;
       display: flex;
+      white-space: nowrap;
+      align-items: center;
     }
     
     .buttonStyle {
@@ -225,7 +227,7 @@ const SiteUpdateExport = styled.div`
       font-weight: bold;
       text-align: right;
       color: white;
-      padding: 10px 0px 10px 200px;
+      padding: 10px 0px 10px 180px;
       margin-top: -3px;
       text-transform: uppercase;
       display: flex;
@@ -249,7 +251,7 @@ const SiteUpdateExport = styled.div`
       border-bottom: 35px solid #004187;
       border-left: 35px solid transparent;
       height: 0;
-      width: 159px;
+      width: 142px;
     }    
 `;
 
@@ -453,82 +455,86 @@ const SiteUpdateResult = ({
       f();
     }, [isFetching]);
 
-    const handleExport = (idx) => {
-      const img = document.createElement("img");
-      img.src = NCILogoExport;
-      img.width = '1';
-      const element = document.getElementById(`${idx}_desc`);
-      const elementClone = element.cloneNode(true);
-      const titleDiv = document.getElementById(`${idx}_title`);
-      const dateDiv = document.getElementById(`${idx}_date`);
-      const newDiv = document.createElement("div");
-      const newDivTitle = document.createElement("div");
-      newDivTitle.style = "display: flex;margin-bottom: 15px;";
-      const titleSpan = document.createElement('span');
-      titleSpan.style = "color: #004187;font-family: Inter;font-size: 28px;font-weight:600;";
-      titleSpan.appendChild(document.createTextNode("Site Update Release Notes"));
-      newDivTitle.appendChild(titleSpan);
-      const newDivUpdate = document.createElement("div");
-      newDivUpdate.style = "display: flex;";
-      const updateSpan = document.createElement('span');
-      updateSpan.style = "color: #567aac;font-family: Inter;font-size: 14px;line-height: 25px;";
-      updateSpan.appendChild(document.createTextNode("UPDATE TITLE:"));
-      const updateSpanValue = document.createElement('span');
-      updateSpanValue.style = "margin-left: 45px;color: #004187;font-family: Inter;font-size: 16px;font-weight:600;line-height: 25px;";
-      updateSpanValue.appendChild(document.createTextNode(titleDiv.innerText));
-      newDivUpdate.appendChild(updateSpan);
-      newDivUpdate.appendChild(updateSpanValue);
-      const newDivDate = document.createElement("div");
-      newDivDate.style = "display: flex;";
-      const dateSpan = document.createElement('span');
-      dateSpan.style = "color: #567aac;font-family: Inter;font-size: 14px;line-height: 25px;";
-      dateSpan.appendChild(document.createTextNode("DATE OF RELEASE:"));
-      const dateSpanValue = document.createElement('span');
-      dateSpanValue.style = "margin-left: 20px;color: #004187;font-family: Inter;font-size: 16px;font-weight:600;line-height: 25px;";
-      dateSpanValue.appendChild(document.createTextNode(dateDiv.innerText));
-      newDivDate.appendChild(dateSpan);
-      newDivDate.appendChild(dateSpanValue);
-      const breakline = document.createElement("HR");
-      breakline.style = "height: 1px; background-color: #3b6697; margin-bottom: 40px;";
-      newDiv.appendChild(newDivTitle);
-      newDiv.appendChild(newDivUpdate);
-      newDiv.appendChild(newDivDate);
-      newDiv.appendChild(breakline);
-      newDiv.appendChild(elementClone);
-      const opt = {
-        margin: [35, 15, 20, 15],
-        filename: "siteupdate_export.pdf",
-        image: {type: 'jpeg', quality: 1},
-        html2canvas: {dpi: 72, scale: 4, letterRendering: true},
-        jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait'}
-      };
-
-      html2pdf().from(newDiv).set(opt).toContainer()
-      .toCanvas()
-      .toPdf()
-      .get('pdf')
-      .then((pdf) => {
-        const totalPages = pdf.internal.getNumberOfPages();
-        for (let i = 1; i <= totalPages; i += 1) {
-            pdf.setPage(i);
-            pdf.addImage(img, 'PNG', 13, 7, 120, 15);
-            pdf.setDrawColor("#606061");
-            pdf.setLineWidth(1.0);
-            pdf.line(15, 27, 195, 27);
-            pdf.setDrawColor("#3b6697");
-            pdf.setLineWidth(0.2);
-            pdf.line(15, 280, 195, 280);
-            pdf.setFontSize(8);
-            pdf.setFont(pdf.getFont().fontName, "normal");
-            pdf.setTextColor("#000000");
-            pdf.text('U.S. Department of Health and Human Services | National Institutes of Health | National Cancer Institute', 35,
-                pdf.internal.pageSize.getHeight() / 1.04);
-            pdf.setFont(pdf.getFont().fontName, "bold");
-            pdf.text(`Page ${i} of ${totalPages}`, 180, pdf.internal.pageSize.getHeight() / 1.04);
-        }
-        })
-        .save();
+    const handleViewPDF = () => {
+        window.open('/CCDC_Release_Notes.pdf', '_blank');
     };
+
+    // const handleExport = (idx) => {
+    //   const img = document.createElement("img");
+    //   img.src = NCILogoExport;
+    //   img.width = '1';
+    //   const element = document.getElementById(`${idx}_desc`);
+    //   const elementClone = element.cloneNode(true);
+    //   const titleDiv = document.getElementById(`${idx}_title`);
+    //   const dateDiv = document.getElementById(`${idx}_date`);
+    //   const newDiv = document.createElement("div");
+    //   const newDivTitle = document.createElement("div");
+    //   newDivTitle.style = "display: flex;margin-bottom: 15px;";
+    //   const titleSpan = document.createElement('span');
+    //   titleSpan.style = "color: #004187;font-family: Inter;font-size: 28px;font-weight:600;";
+    //   titleSpan.appendChild(document.createTextNode("Site Update Release Notes"));
+    //   newDivTitle.appendChild(titleSpan);
+    //   const newDivUpdate = document.createElement("div");
+    //   newDivUpdate.style = "display: flex;";
+    //   const updateSpan = document.createElement('span');
+    //   updateSpan.style = "color: #567aac;font-family: Inter;font-size: 14px;line-height: 25px;";
+    //   updateSpan.appendChild(document.createTextNode("UPDATE TITLE:"));
+    //   const updateSpanValue = document.createElement('span');
+    //   updateSpanValue.style = "margin-left: 45px;color: #004187;font-family: Inter;font-size: 16px;font-weight:600;line-height: 25px;";
+    //   updateSpanValue.appendChild(document.createTextNode(titleDiv.innerText));
+    //   newDivUpdate.appendChild(updateSpan);
+    //   newDivUpdate.appendChild(updateSpanValue);
+    //   const newDivDate = document.createElement("div");
+    //   newDivDate.style = "display: flex;";
+    //   const dateSpan = document.createElement('span');
+    //   dateSpan.style = "color: #567aac;font-family: Inter;font-size: 14px;line-height: 25px;";
+    //   dateSpan.appendChild(document.createTextNode("DATE OF RELEASE:"));
+    //   const dateSpanValue = document.createElement('span');
+    //   dateSpanValue.style = "margin-left: 20px;color: #004187;font-family: Inter;font-size: 16px;font-weight:600;line-height: 25px;";
+    //   dateSpanValue.appendChild(document.createTextNode(dateDiv.innerText));
+    //   newDivDate.appendChild(dateSpan);
+    //   newDivDate.appendChild(dateSpanValue);
+    //   const breakline = document.createElement("HR");
+    //   breakline.style = "height: 1px; background-color: #3b6697; margin-bottom: 40px;";
+    //   newDiv.appendChild(newDivTitle);
+    //   newDiv.appendChild(newDivUpdate);
+    //   newDiv.appendChild(newDivDate);
+    //   newDiv.appendChild(breakline);
+    //   newDiv.appendChild(elementClone);
+    //   const opt = {
+    //     margin: [35, 15, 20, 15],
+    //     filename: "siteupdate_export.pdf",
+    //     image: {type: 'jpeg', quality: 1},
+    //     html2canvas: {dpi: 72, scale: 4, letterRendering: true},
+    //     jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait'}
+    //   };
+
+    //   html2pdf().from(newDiv).set(opt).toContainer()
+    //   .toCanvas()
+    //   .toPdf()
+    //   .get('pdf')
+    //   .then((pdf) => {
+    //     const totalPages = pdf.internal.getNumberOfPages();
+    //     for (let i = 1; i <= totalPages; i += 1) {
+    //         pdf.setPage(i);
+    //         pdf.addImage(img, 'PNG', 13, 7, 120, 15);
+    //         pdf.setDrawColor("#606061");
+    //         pdf.setLineWidth(1.0);
+    //         pdf.line(15, 27, 195, 27);
+    //         pdf.setDrawColor("#3b6697");
+    //         pdf.setLineWidth(0.2);
+    //         pdf.line(15, 280, 195, 280);
+    //         pdf.setFontSize(8);
+    //         pdf.setFont(pdf.getFont().fontName, "normal");
+    //         pdf.setTextColor("#000000");
+    //         pdf.text('U.S. Department of Health and Human Services | National Institutes of Health | National Cancer Institute', 35,
+    //             pdf.internal.pageSize.getHeight() / 1.04);
+    //         pdf.setFont(pdf.getFont().fontName, "bold");
+    //         pdf.text(`Page ${i} of ${totalPages}`, 180, pdf.internal.pageSize.getHeight() / 1.04);
+    //     }
+    //     })
+    //     .save();
+    // };
 
   const handleClick = (idx) => {
     const newOpen = Object.assign([], open);
@@ -623,11 +629,11 @@ const SiteUpdateResult = ({
                     </DataContentType>
                   )}
                   <SiteUpdateExport>
-                    <a href="#" role="button" className="buttonStyle" onClick={() => handleExport(`post${siteUpdateList[selectedIdx].id}`)}>
+                    <a href="#" role="button" className="buttonStyle" onClick={() => handleViewPDF()}>
                       <span className="spanText">
                         <img src={ReleaseNoteDownloadIcon} alt="download icon" />
                         &nbsp;&nbsp;
-                        <span style={{ paddingTop: '3px' }}>DOWNLOAD</span>
+                        <span>VIEW PDF</span>
                       </span>
                     </a>
                   </SiteUpdateExport>
